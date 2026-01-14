70a5f61158effbde79257a26aff44289027b1d29
HyNS00 <hsk7953@gmail.com>
2026-01-14 19:16:13 +0900

    first init

---
+++ src/main/java/com/example/pr_webhook/PrWebhookApplication.java
@@ -0,0 +1,13 @@
+package com.example.pr_webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrWebhookApplication.class, args);
+	}
+
+}
